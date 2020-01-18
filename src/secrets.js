4817d8f0727515c893c7e82a27a44860ee22c257
Kate Howard <k.l.howard.1992@gmail.com>
2020-01-18 20:36:04 +0000

    Bit of a hack

--- src/secrets.js
+++ src/secrets.js
@@ -1,3 +1,3 @@
 export const client_id = '880d5838eb144bcfa727d44849ecd273';
-export const redirect_uri = 'https://rock-n-roll-roulette.herokuapp.com';
+export const redirect_uri = 'http://rock-n-roll-roulette.herokuapp.com';
 export const playlist_id = '7lBs81twGQ54rImM9mJvpv';

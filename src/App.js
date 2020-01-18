e92c3c5916ff1086f81686026b428d17d8d6bcc5
Kate Howard <k.l.howard.1992@gmail.com>
2020-01-18 19:42:48 +0000

    Add auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import { access_token, playlist_id } from './secrets';
+import { client_id, redirect_uri, playlist_id } from './secrets';
 
 function App() {
   const [tracks, setTracks] = useState([]);
   const [currentTrack, setCurrentTrack] = useState({});
+  const [token, setToken] = useState(null);
+  const scopes = ['user-read-currently-playing', 'user-read-playback-state'];
 
   function generate() {
     if (tracks?.length) {
@@ -12,40 +14,98 @@ function App() {
     }
   }
 
-  useEffect(() => {
+  function getAllTracks() {
     const endpoint = `https://api.spotify.com/v1/playlists/${playlist_id}/tracks`;
-    fetch(endpoint, {
-      method: 'get',
-      headers: {
-        'Content-type': 'application/json',
-        Authorization: `Bearer ${access_token}`
-      }
-    })
-      .then(blob => blob.json())
-      .then(data => {
-        const newTracks = data.items.map(item => {
-          return {
-            song: item.track.name,
-            artist: item.track.artists.map(artist => artist.name).join(', '),
-            uri: item.track.uri
-          };
+    if (token) {
+      fetch(endpoint, {
+        method: 'get',
+        headers: {
+          'Content-type': 'application/json',
+          Authorization: `Bearer ${token}`
+        }
+      })
+        .then(blob => blob.json())
+        .then(data => {
+          if (data?.items) {
+            const newTracks = data.items.map(item => {
+              return {
+                song: item.track.name,
+                artist: item.track.artists.map(artist => artist.name).join(', '),
+                uri: item.track.uri
+              };
+            });
+            setTracks(newTracks);
+          }
         });
-        setTracks(newTracks);
-      });
+    }
+  }
+
+  function generateRandomString(length) {
+    let text = '';
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    for (let i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  }
+
+  function getHashParams() {
+    const hashParams = {};
+    const r = /([^&;=]+)=?([^&;]*)/g;
+    const q = window.location.hash.substring(1);
+    let e = r.exec(q);
+    while (e) {
+      hashParams[e[1]] = decodeURIComponent(e[2]);
+      e = r.exec(q);
+    }
+    return hashParams;
+  }
+
+  function handleRedirect(event) {
+    event.preventDefault();
+    const params = getHashParams();
+    const token = params.access_token;
+    setToken(token);
+    const state = generateRandomString(16);
+    let url = 'https://accounts.spotify.com/authorize';
+    url += '?response_type=token';
+    url += '&client_id=' + encodeURIComponent(client_id);
+    url += '&scope=' + encodeURIComponent(scopes);
+    url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+    url += '&state=' + encodeURIComponent(state);
+    window.location = url;
+  }
+
+  useEffect(() => {
+    const params = getHashParams();
+    const token = params.access_token;
+    setToken(token);
   }, []);
 
+  useEffect(() => {
+    if (token) getAllTracks();
+  }, [token]);
+
   return (
-    <React.Fragment>
+    <div className="App">
       <h1>
         Rock 'n' Roll!{' '}
         <span role="img" aria-label="musical notes">
           🎶
         </span>
       </h1>
-      <button onClick={generate}>Get new song</button>
-      <h2>{currentTrack?.song}</h2>
-      <h3>{currentTrack?.artist}</h3>
-    </React.Fragment>
+      {token ? (
+        <div>
+          <button onClick={generate}>Get new song</button>
+          <h2>{currentTrack?.song}</h2>
+          <h3>{currentTrack?.artist}</h3>
+        </div>
+      ) : (
+        <button onClick={event => handleRedirect(event)}>
+          <strong>Log in to Spotify</strong>
+        </button>
+      )}
+    </div>
   );
 }
 
